88190cb362be52db82c07b45b9be376e3f5894c7
Ole Snyders <124588267+IBimsHedebe@users.noreply.github.com>
2025-06-25 21:57:53 +0200

    starting

--- Assets/Scene/Game/World_Gen.cs
+++ Assets/Scene/Game/World_Gen.cs
@@ -2,50 +2,31 @@ using UnityEngine;
 
 public class World_Gen : MonoBehaviour
 {
-    public GameObject[] platformPrefabs; // Asigning all Platform Prefabs
-    public int numberOfPlatforms = 60;
-    public float levelWidth = 10f;
-    public float minY = 1f;
-    public float maxY = 3f;
-    public GameObject[] perkPrefabs; // Asigning all Perk Prefabs
-    public float perkProp = 0.3f;
+    public int width = 50;
+    public int height = 50;
+    public GameObject tile;
 
-    private Vector3 spawnPosition = Vector3.zero;
+    private int[,] dungeonGrid;
 
     void Start()
     {
-        Generator();
+        GenerateLayout();
+        DrawDungeon();
     }
 
-    void Generator()
+    void GenerateLayout()
     {
-        for (int i = 0; i < numberOfPlatforms; i++)
+        dungeonGrid = new int[width, height];
+        // Place rooms, connect with corridors...
+    }
+
+    void DrawDungeon()
+    {
+        for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
         {
-            // Generate Platforms
-            GameObject platform = Instantiate(
-                platformPrefabs[Random.Range(0, platformPrefabs.Length)],
-                spawnPosition,
-                Quaternion.identity
-            );
-
-            platform.transform.parent = transform; //Parenting it, to keep the Hirachy cleaner
-
-            //Move to the next spawn location
-            spawnPosition.x += Random.Range(1f, levelWidth);
-            spawnPosition.y += Random.Range(minY, maxY);
-
-            //Generate Perks
-            float randomValue = Random.Range(0f, 1f);
-
-            if (randomValue < perkProp)
-            {
-                GameObject perks = Instantiate(
-                    perkPrefabs[Random.Range(0, perkPrefabs.Length)], // Proüabillizys are 1/3 for a perk to spawn
-                    new Vector3(spawnPosition.x, spawnPosition.y + 4),
-                    Quaternion.identity
-                );
-                perks.transform.parent = transform; //Parneting it, to keep the Hirachy cleaner
-            }
+            Vector3 pos = new Vector3(x, y, 0);
+            Instantiate(tile, pos, Quaternion.identity);
         }
     }
 }
